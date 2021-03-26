25aa09c118bf1e3fca2a23dec548ac71005863ce
Fishermanykx <fishermanykx.mail@gmail.com>
2021-03-26 08:54:00 +0800

    First successful run

---
+++ build/src/CMakeFiles/demoPass1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demoPass1.dir/demoPass1.o"
+  "libdemoPass1.pdb"
+  "libdemoPass1.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demoPass1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- demoPass1.cpp
+++
@@ -1,7 +0,0 @@
-/*
- * @Description: A Simple demo pass for learning llvm
- * @Author: Fishermanykx
- * @Date: 2021-03-25 15:38:46
- * @LastEditors: Fishermanykx
- * @LastEditTime: 2021-03-25 15:39:24
- */
